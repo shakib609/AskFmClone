765eaa737630af630503c898f741c9c54942dbb5
Shakib Hossain <shakib609@gmail.com>
2016-08-10 13:55:19 +0600

    Fix Question model

--- askfmclone/question/models.py
+++ askfmclone/question/models.py
@@ -4,19 +4,17 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
     text = models.CharField(max_length=300, default=None)
-    asked_to = models.ForeignKey(User,
-                                 on_delete=models.CASCADE,
-                                 related_name='asked_user')
-    asked_by = models.OneToOneField(User,
-                                    default=None,
-                                    related_name='asking_user')
+    asked_to = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name='asked_to_questions')
+    asked_by = models.ForeignKey(User, default=None, null=True,
+                                 related_name='asked_by_questions')
 
     def __str__(self):
         return self.text[:40]
 
 
 class Answer(models.Model):
-    text = models.TextField(default=None)
+    text = models.TextField(default=None, null=True)
     question = models.OneToOneField(Question, on_delete=models.CASCADE)
 
     def __str__(self):
