4e5aa7f5f51e395ac791509b76f71e9ddedab999
Shakib Hossain <shakib609@gmail.com>
2016-08-11 20:44:25 +0600

    Change some settings to make staticfiles work

--- config/settings/common.py
+++ config/settings/common.py
@@ -94,8 +94,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+# Static Files Settings
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(APPS_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,3 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Get database settings from heroku environment
 DATABASES['default'] = dj_database_url.config(conn_max_age=500)
-
-# Static File specific changes for heroku deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
