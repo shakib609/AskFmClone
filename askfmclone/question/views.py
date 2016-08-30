cefa6c0ecf8e3246b4c20db1178cd073b6824745
Shakib Hossain <shakib609@gmail.com>
2016-08-30 23:00:39 +0600

    Make question:detail view restricted to asked_by User only

--- askfmclone/question/views.py
+++ askfmclone/question/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 
 from .models import Question
 
 
 def details(request, question_id):
-    question = get_object_or_404(Question, id=question_id)
-    context = {
-        'question': question
-    }
-    return render(request, 'question/details.html', context)
+    if request.user.is_authenticated():
+        question = get_object_or_404(
+            Question,
+            id=question_id,
+            asked_by=request.user
+        )
+        context = {
+            'question': question
+        }
+        return render(request, 'question/details.html', context)
+    else:
+        messages.error(request, 'Something went wrong!', extra_tags='danger')
+        return redirect(reverse('askfm:my_profile_view'))
