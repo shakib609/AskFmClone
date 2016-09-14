ca257d6a5f3e950b6e18a7541055b1018dccb7be
Shakib Hossain <shakib609@gmail.com>
2016-09-15 03:18:34 +0600

    Using list-group for listing questions and also many interface changes in my_profile and user_profile

--- askfmclone/static/js/modal.js
+++ askfmclone/static/js/modal.js
@@ -3,9 +3,11 @@
         var button = $(e.relatedTarget),
             questionText = button.data('questiontext'),
             questionId = button.data('questionid'),
+            questionAskedBy = button.data('questionaskedby'),
             modal = $(this);
         modal.find('#question-text')
             .text(questionText);
         modal.find('#question-id').val(questionId);
+        modal.find('#question-asked-by').text(questionAskedBy);
     })
 })();
